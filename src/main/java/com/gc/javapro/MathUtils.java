f1f4fe733c73293194442609e2bb3c06bc14cc56
Gaurav Chhabra <gc1133@gmail.com>
2021-11-10 22:04:07 +0530

    final version

--- src/main/java/com/gc/javapro/MathUtils.java
+++ src/main/java/com/gc/javapro/MathUtils.java
@@ -5,4 +5,12 @@ public class MathUtils {
     public Integer sum(int a, int b){
         return a+b;
     }
+
+    public int divide(int a, int b){
+        return a/b;
+    }
+
+    public int multiply(int a, int b) {
+        return a*b;
+    }
 }
--- src/test/java/com/gc/javapro/MathUtilsTest.java
+++ src/test/java/com/gc/javapro/MathUtilsTest.java
@@ -1,14 +1,94 @@
 package com.gc.javapro;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+//@TestInstance(TestInstance.Lifecycle.PER_CLASS) -- is the default one
 class MathUtilsTest {
 
+    MathUtils mathUtils;
+    TestInfo testInfo;
+    TestReporter testReporter;
+
+    @BeforeAll
+    static void beforeAllInit(){
+        System.out.println("This has to run before all...");
+    }
+
+    @BeforeEach
+    void init(TestInfo testInfo, TestReporter testReporter){
+        mathUtils = new MathUtils();
+
+        this.testInfo = testInfo;
+        this.testReporter = testReporter;
+        testReporter.publishEntry("Running " + testInfo.getDisplayName() + " with tags " + testInfo.getTags());
+    }
+
+    @AfterEach
+    void cleanUp(){
+        System.out.println("Cleaning up...");
+    }
+
     @Test
-    void sum() {
-        MathUtils mathUtils = new MathUtils();
-        assertEquals(2, mathUtils.sum(1,1));
+    @DisplayName("Testing sum method")
+    void testSum() {
+        Integer expected = 2;
+        Integer actual = mathUtils.sum(1, 1);
+        assertEquals(expected, actual, "Sum method should add the two numbers");
+    }
+
+    @Test
+    @Tag("Math")
+    public void testDivide(){
+        assertThrows(ArithmeticException.class, () -> mathUtils.divide(2,0), "Divide by zero throws exception");
+        //assertThrows(NullPointerException.class, () -> mathUtils.divide(2,0), "Divide by zero throws exception");
+    }
+
+    @Test
+    @DisplayName("TDD. This method is disabled as of now")
+    @Disabled
+    void testDisabled(){
+        fail("This test should fail");
+    }
+
+    @Nested
+    @DisplayName("add method")
+    class AddTest{
+
+        @Test
+        @DisplayName("when adding positive numbers")
+        void addPositiveNumbers(){
+            assertEquals(3, mathUtils.sum(1,2), "Should return the right sum");
+        }
+
+        @Test
+        @DisplayName("when adding negative numbers")
+        void addNegativeNumbers(){
+            assertEquals(-3, mathUtils.sum(-1, -2),"Should return the right sum");
+        }
+
+    }
+
+    @Test
+    @DisplayName("test multiply method")
+    @Tag("Math")
+    void testMultiply(){
+        assertAll(
+                () -> assertEquals(4,mathUtils.multiply(2,2)),
+                () -> assertEquals(0,mathUtils.multiply(2,0)),
+                () -> assertEquals(-2,mathUtils.multiply(-2,1))
+        );
+
+    }
+
+    @Test
+    void testOptimizedMultiply(){
+        int expected = 4;
+        int actual = mathUtils.multiply(2,2);
+
+        // here failure message string is evaluated only upon failure
+        assertEquals(expected, actual, () -> "should return " + expected + " but returned " + actual );
     }
 }
